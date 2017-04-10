c4b9ce8c09bf6339d091a20e12c1d4e131cb8d50
poldrack <poldrack@gmail.com>
2017-04-10 02:06:50 -0700

    adding lm

--- somecode.R
+++ somecode.R
@@ -1,3 +1,5 @@
 df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
               header=TRUE,sep=',')
 head(df)
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
