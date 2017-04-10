dd324d9eee428c6961da154c89eeba0ed6b25fdb
Russ Poldrack <poldrack@gmail.com>
2017-04-10 10:16:17 -0700

    Update somecode.R

--- somecode.R
+++ somecode.R
@@ -1,5 +1,13 @@
 df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
               header=TRUE,sep=',')
 head(df)
+
+max_age=120
+min_age=18
+
+stopifnot(max(df$age)<max_age)
+stopifnot(min(df$age)>min_age)
+
+
 lm.result=lm(conspiracist_avg~age,data=df)
 summary(lm.result)
